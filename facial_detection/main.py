f16e6d1bfdca448246d5c3b3b7301bee3a0edd62
Nbiish <K3NW48@users.noreply.github.com>
2023-03-01 05:18:23 +0000

    prepared the dockerfile and added a sound to play

---
+++ facial_detection/main.py
@@ -0,0 +1,24 @@
+import cv2
+from playsound import playsound
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+    if len(faces) > 0:
+        print("Face detected!")
+        playsound("i_see_you_sound.mp3")
+
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
